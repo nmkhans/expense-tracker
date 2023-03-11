35b554ce0ca23a8f5bc2edd6ff95d36d4e2be873
nmkhans <nurmoin05@gmail.com>
2023-03-12 00:09:38 +0600

    some class issue fixed

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import editImage from "../../assets/edit.svg";
-import deleteImage from "../../assets/delete.svg";
+import TransactionListItem from "./../TransactionListItem/TransactionListItem";
 
 const TransactionList = () => {
   return (
     <>
-      <p class="second_heading">Your Transactions:</p>
-      <div class="conatiner_of_list_of_transactions">
+      <p className="second_heading">Your Transactions:</p>
+      <div className="conatiner_of_list_of_transactions">
         <ul>
-          <li class="transaction income">
-            <p>Earned this month</p>
-            <div class="right">
-              <p>৳ 100</p>
-              <button class="link">
-                <img class="icon" src={editImage} />
-              </button>
-              <button class="link">
-                <img class="icon" src={deleteImage} />
-              </button>
-            </div>
-          </li>
+          <TransactionListItem />
+          <TransactionListItem />
         </ul>
       </div>
     </>
---
+++ src/components/TransactionListItem/TransactionListItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import editImage from "../../assets/edit.svg";
+import deleteImage from "../../assets/delete.svg";
+
+const TransactionListItem = () => {
+  return (
+    <li className="transaction income">
+      <p>Earned this month</p>
+      <div className="right">
+        <p>৳ 100</p>
+        <button className="link">
+          <img className="icon" src={editImage} />
+        </button>
+        <button className="link">
+          <img className="icon" src={deleteImage} />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TransactionListItem;
